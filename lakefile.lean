bb238a17c479fdf80b78d11ae532b83167d1b776
Joe Hendrix <joe@lean-fro.org>
2023-10-22 19:48:02 -0700

    Use pkg-config for library path.

--- lakefile.lean
+++ lakefile.lean
@@ -8,10 +8,21 @@ package LibUV where
 
 module_data alloy.c.o : BuildJob FilePath
 
+def runPkgConfig (args : Array String) : IO (Array String) := do
+  let out ← IO.Process.output { cmd := "pkg-config", args }
+  if out.exitCode ≠ 0 then
+    let stderr ← IO.getStderr
+    stderr.putStr s!"Cannot run pkg-config"
+    stderr.flush
+    IO.Process.exit 1
+  return out.stdout.splitOn.toArray.map String.trimRight
+
+def pkgConfigCFlags (lib : String) : IO (Array String) := runPkgConfig #["--cflags", lib]
+def pkgConfigLibs (lib : String) : IO (Array String) := runPkgConfig #["--libs", lib]
+
 @[default_target]
-lean_lib LibUV {
+lean_lib LibUV where
   precompileModules := true
   nativeFacets := #[Module.oFacet, `alloy.c.o]
-  moreLeancArgs := #["-I/opt/homebrew/include"] 
-  moreLinkArgs := #["-L/opt/homebrew/lib", "-luv"]
-}
+  moreLeancArgs := run_io (pkgConfigCFlags "libuv")
+  moreLinkArgs  := run_io (pkgConfigLibs   "libuv")
