c53c349a64b650c3c788cb21457f75b9736d04e6
Joe Hendrix <joe@lean-fro.org>
2023-12-19 19:10:35 -0800

    chore: Cleanup examples

--- examples/timer.lean
+++ examples/timer.lean
@@ -4,15 +4,18 @@ def main : IO Unit := UV.IO.run do
   let l ← UV.mkLoop
   let t ← l.mkTimer
   let start ← l.now
+  let inc := 10
+  let maxInc := 15
+  let dur := 100
   let e := start + 100
   let last ← IO.mkRef start
-  UV.log s!"Timer started"
-  t.start 10 10 do
+  UV.log s!"Timer will step every {inc}ms and stop after {dur}ms."
+  t.start inc inc do
     let now ← l.now
     let dur := now - (←last.get)
     last.set now
-    UV.log s!"Elapsed {dur}"
-    if dur < 10 || dur > 15 then
+    UV.log s!"Elapsed {now - start}ms"
+    if dur < inc || dur > maxInc then
       UV.fatal s!"Unexpected duration {dur}."
     if now ≥ e then
       t.stop
